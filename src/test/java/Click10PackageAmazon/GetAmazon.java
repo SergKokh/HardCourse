4f606e4fcd93834008897fc1ea2fd908898eb153
Serg <kohan_sergey@ukr.net>
2022-06-24 17:18:00 +0300

    Last 5 Tests

--- src/test/java/Click10PackageAmazon/GetAmazon.java
+++ src/test/java/Click10PackageAmazon/GetAmazon.java
@@ -5,11 +5,14 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class GetAmazon extends TestInit{
 
     @Test
     public void checkHomeAudioVisibility() {
         navigateAmazon();
+        sleep(2);
         getAllMenu().click();
         getElectronicMenu().click();
     //    getHomeAudioMenu().isDisplayed();
@@ -19,6 +22,7 @@ public class GetAmazon extends TestInit{
     public void checkSelectedProduct(){
         navigateAmazon();
         inputInSearchField().sendKeys("Samsung\n");
+        sleep(2);
         selectProduct1().click();
         productInDisplayed().isDisplayed();
         Assert.assertTrue(productInDisplayed().isDisplayed());
@@ -77,30 +81,98 @@ public class GetAmazon extends TestInit{
         getAllMenu().click();
         sleep(2);
         getElectronicMenu().click();
-        getCallPhoneAhdAcces().click();
+        getCallPhoneAndAcces().click();
         sleep(2);
         getCallPhone().click();
+        sleep(2);
         selectCallPhone().click();
+        sleep(2);
         Assert.assertTrue(chouseCallPhone().isDisplayed());
 
     }
 
-    public WebElement chouseCallPhone() {
-        return driver.findElement(By.xpath("//span[normalize-space()='Apple iPhone 11, 64GB, Black - Unlocked (Renewed)']"));
+    @Test
+    public void addToCart(){
+        navigateAmazon();
+        changeRegionPoland();
+        sleep(3);
+        goToCallPhonePage();
+        sleep(2);
+        selectCallPhone().click();
+        sleep(2);
+        addProductToCard().click();
+        sleep(3);
+        clickCartBtn().click();
+        Assert.assertTrue(productInCard().isDisplayed());
+
+    }
+    @Test
+    public void checkCleanCart(){
+        navigateAmazon();
+        changeRegionPoland();
+        sleep(3);
+        goToCallPhonePage();
+        sleep(2);
+        selectCallPhone().click();
+        sleep(2);
+        addProductToCard().click();
+        sleep(3);
+        clickCartBtn().click();
+        deleteProductFromCart().click();
+        sleep(2);
+        Assert.assertEquals(cartIsEmpty().getText(), "Your Amazon Cart is empty.");
     }
 
-    public WebElement selectCallPhone() {
-        return driver.findElement(By.xpath("//span[normalize-space()='Apple iPhone 11, 64GB, Black - Unlocked (Renewed)']"));
+    public WebElement cartIsEmpty() {
+        return driver.findElement(By.xpath("//h1[@class= \"a-spacing-mini a-spacing-top-base\"]"));
     }
 
-    public WebElement getCallPhone() {
-        return driver.findElement(By.xpath("//a[@data-routing='off']//span[contains(text(),'Cell Phones')]"));
+    public WebElement deleteProductFromCart() {
+        return driver.findElement(By.xpath("//input[@value= \"Delete\"]"));
     }
 
-    public WebElement getCallPhoneAhdAcces() {
-        return driver.findElement(By.xpath("//a[contains(text(), 'Cell Phones & Accessories')]"));
+    @Test
+    public void checkAddList(){
+        navigateAmazon();
+        changeRegionPoland();
+        sleep(2);
+        goToCallPhonePage();
+        sleep(2);
+        selectCallPhone().click();
+        sleep(2);
+        addToList().click();
+        Assert.assertEquals(fieldSignIn().getText(), "Sign-In");
+
     }
 
+    @Test
+    public void checkSearchField(){
+        navigateAmazon();
+        changeRegionPoland();
+        sleep(2);
+        inputInSearchField().sendKeys("computers");
+        clickEnter().click();
+        sleep(3);
+        Assert.assertEquals(wordComputers().getText(), "Computers & Tablets");
+
+    }
+
+    @Test
+    public void changePaymentCurrency(){
+        navigateAmazon();changeRegionPoland();
+        sleep(2);
+        goToCallPhonePage();
+        sleep(3);
+        clickIkonCuntry().click();
+        sleep(2);
+        getMenuCurrency().click();
+        sleep(2);
+        chouseCurrencyEuro().click();
+        sleep(2);
+        clickSaveChange().click();
+        sleep(2);
+        Assert.assertEquals(valueCurrency().getText(), "EUR");
+    }
 
 }
 
